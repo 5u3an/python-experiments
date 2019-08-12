fa90b789b53929b2622e935bee88de86e2192b06
Suman Khadka <suman@ncit.edu.np>
2019-08-12 17:51:23 +0545

    Update hw.py

--- cw1/hw.py
+++ cw1/hw.py
@@ -15,22 +15,26 @@
 # 2
 
 
-a=0
-b=0
+
+a = 0
+b = 0
 while True:
-  r = input("enter the direction step: ")
-  if not r:
-    break
-  dir, steps = r.split(' ')
-  if dir == "left":
-      a = a - int(steps)
-  elif dir == "right":
-      a = a + int(steps)
-  elif dir == "up":
-      b = b + int(steps)
-  elif dir == "down":
-      b = b - int(steps)
-  else:
-    pass
-distance = (a**2 + b**2)**(1/2)
-print("the output is: ",int(distance))
+    r = input("enter the direction step: ")
+    if not r:
+        break
+    try:
+        dire, steps = r.split(' ')
+    except IndentationError:
+        print("Sorry! Something went wrong. Try again.")
+    if dire == "left":
+        a = a - int(steps)
+    elif dire == "right":
+        a = a + int(steps)
+    elif dire == "up":
+        b = b + int(steps)
+    elif dire == "down":
+        b = b - int(steps)
+    else:
+        pass
+distance = (a ** 2 + b ** 2) ** (1 / 2)
+print("the output is: ", int(distance))
